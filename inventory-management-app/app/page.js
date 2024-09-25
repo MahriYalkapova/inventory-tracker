717cb87145e4fd881765a45bfeb91946dd755283
cyberwiz <115326337+MahriYalkapova@users.noreply.github.com>
2024-09-25 12:18:29 -0700

    Created an add button that is able to add to the database and display on webpage

--- inventory-management-app/app/page.js
+++ inventory-management-app/app/page.js
@@ -1,22 +1,57 @@
 "use client"
-import { Box, Stack, Typography } from "@mui/material";
+import { Box, Stack, Typography, Button, Modal, TextField } from "@mui/material";
 import { firestore } from "@/firebase";
-import { collection, getDocs, query } from "firebase/firestore";
-import { useEffect } from "react";
+import { collection, getDocs, query, doc, setDoc } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { set, update } from "firebase/database";
 
-const item = ['tomato', 'potato', 'onion', 'garlic', 'ginger', 'carrot', 'lettuce', 'kale', 'cucumber']
+//const item = ['tomato', 'potato', 'onion', 'garlic', 'ginger', 'carrot', 'lettuce', 'kale', 'cucumber']
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'white',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+  display: 'flex',
+  flexDirection: 'column',
+  gap: 3,
+};
 
 export default function Home() {
+  const [pantry, setPantry] = useState([])
+
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const [itemName, setItemName] = useState('')
+
+  const updatePantry = async () => {
+    const snapshot = query(collection(firestore, 'pantry'))
+    const docs = await getDocs(snapshot)
+    const pantryList = []
+    docs.forEach((doc) => {
+      pantryList.push(doc.id)
+    })
+    console.log(pantryList)
+    setPantry(pantryList)
+  }
+
   useEffect(() => {
-    const updatePantry = async () => {
-      const snapshot = query(collection(firestore, 'pantry'))
-      const docs = await getDocs(snapshot)
-      docs.forEach((doc) => {
-        console.log(doc.id)
-      });
-    }
     updatePantry()
   }, [])
+
+  const addItem = async (item) => {
+    const docRef = doc(collection(firestore, 'pantry'), item)
+    await setDoc(docRef, {})
+    updatePantry()
+  }
+
   return <Box 
     width="100vw" 
     height="100vh"
@@ -24,7 +59,32 @@ export default function Home() {
     justifyContent={"center"}
     flexDirection={"column"}
     alignItems={"center"}
+    gap={2}
   >
+    <Modal
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+      >
+      <Box sx={style}>
+        <Typography id="modal-modal-title" variant="h6" component="h2">
+          Add item
+        </Typography>
+        <Stack width="100%" direction={'row'} spacing={2}>
+          <TextField id="outlined-basic" label="Item" variant="outlined" fullWidth 
+          value={itemName}
+          onChange={(e) => setItemName(e.target.value)}/>
+          <Button variant="outlined" 
+          onClick={() => {
+            addItem(itemName)
+            setItemName('')
+            handleClose()
+          }}>Add</Button>
+        </Stack>
+      </Box>
+    </Modal>
+    <Button variant="contained" onClick={handleOpen}>Add</Button>
     <Box border={'1px solid #333'}>
       <Box
         width="800px" 
@@ -39,11 +99,11 @@ export default function Home() {
         </Typography>
       </Box>
       <Stack width="800px" height="300px" spacing={2} overflow={'auto'}>
-        {item.map((i) => (
+        {pantry.map((i) => (
           <Box
             key={i}
             width="100%"
-            height="300px"
+            minHeight="50px"
             display={'flex'}
             justifyContent={'center'}
             alignItems={'center'}
